4028c9496a71912d6c7b5cdbbccabf440c42163d
SadeqSp <sadeq.sp@gmail.com>
2020-08-16 12:07:52 +0430

    first: simple & clean

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const mine = (
+    <main>
+        <span>exploring react</span>
+    </main>
+)
+
+ReactDOM.render(mine, document.getElementById('root'));
