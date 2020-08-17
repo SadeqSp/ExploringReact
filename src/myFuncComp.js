87e3e20967def4d4368fd856a93b2a559efe12c4
SadeqSp <sadeq.sp@gmail.com>
2020-08-17 14:15:47 +0430

    adding state to function component [useState hook]

--- src/myFuncComp.js
+++ src/myFuncComp.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-// Regular Function Component   [props > string/variable]             
+// Stateless Regular Function Component   [props > string/variable]             
 function Func1 (props) {
     return <code>{props.name} - {props.age}</code>;
 }
 
-// Arrow Function Component     [props > object]       
+// Stateless Arrow Function Component     [props > object]       
 const Func2 = (props) =>            
     <code>{props.person.name} - {props.person.age}</code>;
+
+// Statefull Arrow Function Component       (+{useState}hook)
+const Func3 = () => {
+    const [name, setName] = useState ('forth');
+    const [age, setAge] = useState ('15');
+    
+    function changeInfo() {
+        setName('newName');
+        setAge('newAge');
+    }
+    return (
+        <div>
+            <code>{name} - {age}</code>
+            <button onClick={changeInfo}>change info</button>
+        </div>
+    );
+};
 
 function FuncComp () {
     const a = '22 [variable]';
     const b = {name:'third', age: '50 [object]'};
     return (
         <div>
-            <h4> function component:</h4>
+            <h4>function component:</h4>
             <div class='box'>
+                <h5>stateless</h5>
                 <Func1 name='first' age='44 [string]' />
                 <Func1 name='second' age={a} />
                 <Func2 person={b} />
+                <h5>statefull</h5>
+                <Func3 />
             </div>
         </div>
     )
