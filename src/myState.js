1fad2fcee91c1761c3bd53c2f5a9eb7674813aa3
SadeqSp <sadeq.sp@gmail.com>
2020-10-19 14:19:40 +0330

    adding constructor to myState

--- src/myState.js
+++ src/myState.js
@@ -6,6 +6,12 @@ class MyState extends React.Component {
     // without  consturctor
     state = {c1: "white", c2: "red", age: 400};
     
+    // with constructor
+    constructor() {
+        super();
+        this.state = {c1: "white", c2: "red", age: 500};
+    }
+
     // changing state with an event [onClick]
     changeColor = () => {this.setState({c1: this.state.c2, age: 100});}
 
@@ -14,9 +20,9 @@ class MyState extends React.Component {
         return (
             <div>
                 <div class='box'>
-                <h5>state</h5>
-                <code style={color}>{this.state.age}</code>
-                <button type='button' onClick={this.changeColor} >change</button>
+                    <h5>state</h5>
+                    <code style={color}>{this.state.age}</code>
+                    <button type='button' onClick={this.changeColor} >change</button>
                 </div>
             </div>
         )
